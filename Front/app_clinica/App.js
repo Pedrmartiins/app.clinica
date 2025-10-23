8964070ea081de079b5b3e185299e87cece30120
pedro <pedroenriquellopes2011@gmail.com>
2025-10-22 22:40:04 -0300

    update

---
+++ Front/app_clinica/App.js
@@ -0,0 +1,8 @@
+import React, {useState} from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigation} from "@react-navigation/stack";
+
+import Splash from '/src/screens/Splash/Splash';
+
+import MenuScreen from 
